1fdbd2e5470f8e77279c69c70a73608fc41c2f06
Eric L. Hepperle <codewizard13@gmail.com>
2022-04-20 15:03:48 -0500

    INITIAL COMMIT

---
+++ ch_16/app.js
@@ -0,0 +1,37 @@
+ch_title = "16: If Statements & Comparison Operators"
+
+// define elements
+const chapter = document.querySelector('h2');
+chapter.innerHTML = ch_title;
+
+let val;
+
+const today = new Date();
+birthday = new Date('9-10-1981 11:25:00')
+birthday = new Date('9/11/1981')
+
+
+val = today.getMonth();
+val = today.getDate()
+val = today.getDay()
+val = today.getFullYear()
+val = today.getHours()
+val = today.getMinutes()
+val = today.getSeconds()
+val = today.getMilliseconds()
+val = today.getTime(); // unix timestamp
+
+birthday.setMonth(2);
+birthday.setDate(12)
+birthday.setFullYear(1985)
+birthday.setHours(3)
+birthday.setMinutes(30)
+birthday.setSeconds(33)
+
+val = today
+
+
+
+
+
+console.log(val)
