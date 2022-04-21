189d051254b1089d79bc8b436635fe9f38c5f0c1
Eric L. Hepperle <codewizard13@gmail.com>
2022-04-21 10:02:51 -0500

    CH 21:  FINISHED

--- ch_21/app.js
+++ ch_21/app.js
@@ -1,5 +1,5 @@
 sec_num = 2
-ch_title = "CH: SOMETHING"
+ch_title = "21. Block Scope With let & const"
 
 // define elements
 const chapter = document.querySelector('h2');
@@ -10,9 +10,36 @@ sec_el.innerHTML = sec_num
 
 /* --------------------------- */
 
-let val;
+// Global Scope
+var a = 1
+let b = 2
+const c = 3
 
+// function test() {
+//   var a = 4
+//   let b = 5
+//   const c = 6
+//   console.log('Function Scope: ', a, b, c)
 
+// }
 
+// test()
 
-console.log(val)
+// if (true) {
+//   // Block Scope (anything that runs in curly braces)
+//   var a = 4
+//   let b = 5
+//   const c = 6  
+//   console.log('If Scope: ', a, b, c)
+// }
+
+// Var is weird and causes confusion and security risks
+
+// for (let a = 0; a < 10; a++) { // let works
+for (var a = 0; a < 10; a++) { // var changes the value of global a from withing block scope
+  console.log(`Loop: ${a}`)
+}
+
+// Let and const have a block level scope and var has function scope
+
+console.log('Global Scope: ', a, b, c)
