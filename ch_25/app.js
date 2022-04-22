8606b88a22f121657e07e1d5ca1fb70aabd6f0f9
Eric L. Hepperle <codewizard13@gmail.com>
2022-04-22 07:27:50 -0500

    CH 26:  ADD Starting project files

---
+++ ch_25/app.js
@@ -0,0 +1,61 @@
+// 25. DOM Selectors For Multiple Elements
+
+// These return HTML collection or nodelist
+
+// document.getElementsByClassName
+
+// const items = document.getElementsByClassName('collection-item')
+// console.log(items)
+// console.log(items[0])
+// items[0].style.color = 'red'
+// items[3].textContent = 'Hello'
+
+// const listItems = document.querySelector('ul').getElementsByClassName('collection-item')
+
+// console.log(listItems)
+
+// document.getElementsByTagName
+
+// let lis = document.getElementsByTagName('li')
+// console.log(lis)
+// console.log(lis[0])
+// lis[0].style.color = 'red'
+// lis[3].textContent = 'Hello'
+
+// // HTML collection is NOT an array
+
+// // Convert HTML collection into array
+// lis = Array.from(lis)
+
+// lis.reverse()
+
+// lis.forEach(function(li, index){
+//   console.log(li.className)
+//   li.textContent = `${index}: Hello`
+// })
+
+// console.log(lis)
+
+// NODELIST: counts text nodes and does forEach and other array methods
+// without having to convert to array
+
+// document.querySelectorAll() -- returns a nodelist
+
+const items = document.querySelectorAll('ul.collection li.collection-item')
+
+items.forEach(function (item, index) {
+  item.textContent = `${index}: Hello`
+})
+
+const liOdd = document.querySelectorAll('li:nth-child(odd)')
+const liEven = document.querySelectorAll('li:nth-child(even)')
+
+liOdd.forEach(function(li, index){
+  li.style.background = '#ccc'
+})
+
+for (let i = 0; i < liEven.length; i++) {
+  liEven[i].style.background = '#f4f4f4'
+}
+
+console.log(items)
