ac6c7cf781c4273f33e6d8bcd3288a1e81602a98
Eric L. Hepperle <codewizard13@gmail.com>
2022-04-27 14:23:48 -0500

    CH 31:  FINISH

--- ch_31/app.js
+++ ch_31/app.js
@@ -2,11 +2,53 @@
 
 const form = document.querySelector('form')
 const taskInput = document.getElementById('task')
+const heading = document.querySelector('h5')
+const select = document.querySelector('select')
 
-form.addEventListener('submit', runEvent)
+// Clear input
+taskInput.value = ""
+
+// form.addEventListener('submit', runEvent)
+
+// Keydown
+// taskInput.addEventListener('keydown', runEvent)
+
+// Keyup
+// taskInput.addEventListener('keyup', runEvent)
+
+// Keypress
+// taskInput.addEventListener('keypress', runEvent)
+
+// Keypress
+// taskInput.addEventListener('focus', runEvent)
+
+// Blur - oposite of focus
+// taskInput.addEventListener('blur', runEvent)
+
+// Cut (as in cut and paste)
+// taskInput.addEventListener('cut', runEvent)
+
+// Paste
+// taskInput.addEventListener('paste', runEvent)
+
+// Input - Any action in a input field (cut, paste, keydown, keyup, focus, blur, etc.)
+// taskInput.addEventListener('input', runEvent)
+
+// GOTCHA: With Materialize you won't see the select list because of how it works, so just
+// disable the materialize script in index.html temporarily
+
+// Change
+select.addEventListener('change', runEvent)
+
 
 function runEvent(e){
-  e.preventDefault()
-
   console.log(`EVENT TYPE: ${e.type}`)
+
+  console.log(e.target.value) // logs everything we type
+  heading.innerText = e.target.value // everything you type is displayed in the h5 element
+
+  // Get input value
+  // console.log(taskInput.value)
+
+  // e.preventDefault()
 }
