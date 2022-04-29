25789968fbc1e29fdb919574b493b526fda63c32
Eric L. Hepperle <codewizard13@gmail.com>
2022-04-29 11:37:02 -0500

    CH 36:  TASKLIST - Tasks are being persisted to local storage

--- SEC_4/ch_36/tasklist/app.js
+++ SEC_4/ch_36/tasklist/app.js
@@ -45,12 +45,34 @@ function addTask(e) {
   // Append li to ul
   taskList.appendChild(li)
 
+  // Store in Local Storage
+  storeTaskInLocalStorage(taskInput.value)
+
   // Clear input
   taskInput.value = ""
 
 
   e.preventDefault()
 }
+
+
+// Store Task
+function storeTaskInLocalStorage(task) {
+  let tasks
+
+  if (localStorage.getItem('tasks') === null) {
+    tasks = []
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'))
+  }
+
+  tasks.push(task)
+
+  localStorage.setItem('tasks', JSON.stringify(tasks))
+
+}
+
+
 
 // Remove Task
 function removeTask(e) {
