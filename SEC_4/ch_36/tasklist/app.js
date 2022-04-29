a62713da2100a5cc4c1aaa0845d21a8bc82cada7
Eric L. Hepperle <codewizard13@gmail.com>
2022-04-29 13:12:14 -0500

    CH 36:  TASKLIST - Tasks deleted from UI also deleted from localstorage

--- SEC_4/ch_36/tasklist/app.js
+++ SEC_4/ch_36/tasklist/app.js
@@ -114,12 +114,30 @@ function removeTask(e) {
       e.target.parentElement.parentElement.remove()
 
       // Remove from local storage
-      
+      removeTaskFromLocalStorage(e.target.parentElement.parentElement)
 
     }
   }
 
   console.log(e.target)
+}
+
+// Remove from local storage
+function removeTaskFromLocalStorage(taskItem) {
+  if (localStorage.getItem('tasks') === null) {
+    tasks = []
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'))
+  }
+
+  tasks.forEach(function(task, index){
+    if (taskItem.textContent === task) {
+      tasks.splice(index, 1)
+    }
+  })
+
+  localStorage.setItem('tasks', JSON.stringify(tasks))
+
 }
 
 // Clear Tasks
